27301553d94f705a7f94e4e3d1702476f36ebfcc
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-06-07 23:18:46 +0800

    update

--- PyTicTacToe.py
+++ PyTicTacToe.py
@@ -13,6 +13,8 @@ class TicTacToe:
 
         self.Sprite = { "Tile": "Sprite/Tile.png","X": "Sprite/X Tile.png", "O": "Sprite/O Tile.png"}
 
+        self.available_tile = {"a1": 0,"a2":0,"a3":0,"b1": 0,"b2":0,"b3":0,"c1": 0,"c2":0,"c3":0}
+
         self.layout = [
             [self.Tile(self.Sprite["Tile"],"a1"),self.Tile(self.Sprite["Tile"],"a2"),self.Tile(self.Sprite["Tile"],"a3")],
             [self.Tile(self.Sprite["Tile"],"b1"),self.Tile(self.Sprite["Tile"],"b2"),self.Tile(self.Sprite["Tile"],"b3")],
@@ -21,16 +23,26 @@ class TicTacToe:
 
         self.window = sg.Window("PyTicTacToe",layout=self.layout,size=self.window_size)
 
+    def CheckWinner(self,key):
+        pass
+
 
     def ChangeTile(self,key):
 
-        self.Turn += 1
+        if self.available_tile[key] <= 0:
+            self.Turn += 1
+            
+            if self.Turn % 2: #even numbers
+                self.available_tile[key] = 2
 
-        if self.Turn % 2: #even numbers
-            self.window[key].update(filename=self.Sprite["O"])
+                self.window[key].update(filename=self.Sprite["O"])
 
-        else: #odd numbers
-            self.window[key].update(filename=self.Sprite["X"])
+            else: #odd numbers
+                self.available_tile[key] = 1
+                self.window[key].update(filename=self.Sprite["X"])
+
+
+
 
 
     def Tile(self,content,key):
@@ -44,6 +56,7 @@ class TicTacToe:
 
             if event in ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]:
                 self.ChangeTile(event)
+
 
             if event == sg.WINDOW_CLOSED:
                 self.game_running = False
